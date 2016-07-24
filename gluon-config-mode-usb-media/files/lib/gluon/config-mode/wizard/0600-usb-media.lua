1c8ee5b9c05cd1a0990a91b8170192646a93b996
rubo77 <github@r.z11.de>
2016-07-24 14:34:22 +0200

    etc file must be included in the file with default calues

--- gluon-config-mode-usb-media/files/lib/gluon/config-mode/wizard/0600-usb-media.lua
+++ gluon-config-mode-usb-media/files/lib/gluon/config-mode/wizard/0600-usb-media.lua
@@ -7,18 +7,18 @@ local M = {}
 function M.section(form)
   local s = form:section(cbi.SimpleSection, nil, i18n.translate(
     'If you want to share your attached USB storage devices, '
-      .. 'here you can enable this and specify the visable path in '
+      .. 'here you can enable this and specify the visible path in '
       .. 'the URL of your Node.'))
 
 
   local o
 
   o = s:option(cbi.Flag, "_usbmediasharing", i18n.translate("Share your devices"))
-  o.default = uci:get("usb-media", "settings", "share_device", o.disabled)
+  o.default = uci:get("gluon-usb-media", "settings", "share_device", o.disabled)
   o.rmempty = false
 
   o = s:option(cbi.Value, "_usbmediapath", i18n.translate("Path"))
-  o.default = uci:get("usb-media", "settings", "path")
+  o.default = uci:get("gluon-usb-media", "settings", "path")
   o:depends("_usbmediasharing", "1")
   o.rmempty = false
   o.datatype = "hostname"
@@ -28,20 +28,20 @@ end
 
 function M.handle(data)
 
-  uci:section('usb-media', 'usb-media', 'settings',
+  uci:section('gluon-usb-media', 'gluon-usb-media', 'settings',
             {
                     share_device = data._usbmediasharing,
             }
   )
   if data._usbmediasharing and data._usbmediapath ~= nil then
-    uci:section('usb-media', 'usb-media', 'settings',
+    uci:section('gluon-usb-media', 'gluon-usb-media', 'settings',
 	    {
 		    path = data._usbmediapath,
 	    }
     )
   end
-  uci:save("usb-media")
-  uci:commit("usb-media")
+  uci:save("gluon-usb-media")
+  uci:commit("gluon-usb-media")
 end
 
 return M
